c8ba14826a3a85a779fb7ff903eba4ca3c36f5b4
SravanMuddamsetty <76273242+SravanMuddamsetty@users.noreply.github.com>
2026-05-25 21:33:09 -0400

    Phase 5-Task 10: Display of Appointments

--- src/app/features/appointments/models/appointments.interface.ts
+++ src/app/features/appointments/models/appointments.interface.ts
@@ -3,14 +3,11 @@ import { Patient } from '../../patients/models/patients.interface';
 
 export interface Appointment {
   id: number;
-
   patientId: number;
   doctorId: number;
-
   appointmentDate: string;
   reason: string;
   status: 'Scheduled' | 'Completed' | 'Cancelled';
-
   patient?: Patient;
   doctor?: Doctor;
 }
